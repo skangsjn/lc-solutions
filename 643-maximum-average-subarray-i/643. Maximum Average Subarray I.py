54e5ac7f7ef0529636899b018eef2ae5dbc3a4f1
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-02 00:53:00 -0400

    Runtime: 91 ms (19.71%), Memory: 29.5 MB (7.31%) - Gitcode

--- 643-maximum-average-subarray-i/643. Maximum Average Subarray I.py
+++ 643-maximum-average-subarray-i/643. Maximum Average Subarray I.py
@@ -1,20 +1,20 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        summation = start = end = 0
+        curr = start = end = 0
         
         for i in range(k):
-            summation += nums[i]
+            curr += nums[i]
             end += 1
-        max_avg = summation / k
+        ans = curr
         
         while end <= len(nums)-1:
-            summation += nums[end] - nums[start]
-            max_avg = max(max_avg, summation / k)
+            curr += nums[end] - nums[start]
+            ans = max(ans, curr)
             
             start += 1
             end += 1
             
-        return max_avg
+        return ans / k
 
             
             
