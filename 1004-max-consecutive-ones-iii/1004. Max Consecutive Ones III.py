783dd8c3dd99b9751ea0a7164cb43ba5c0652c67
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-02 00:45:48 -0400

    Runtime: 81 ms (11.27%), Memory: 22.2 MB (22.30%) - Gitcode

---
+++ 1004-max-consecutive-ones-iii/1004. Max Consecutive Ones III.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        start = end = flips = ans = 0
+
+        while end < len(nums):
+            if nums[end] == 0:
+                flips += 1
+                            
+            while flips > k:
+                if nums[start] == 0:
+                    flips -= 1
+                start += 1
+            
+            ans = max(ans, end - start + 1)
+            end += 1 
+                            
+        return ans
