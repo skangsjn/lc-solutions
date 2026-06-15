8b1d61c4250d4f1b3c567581bbe314023018ccf2
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-14 23:50:05 -0400

    Runtime: 15 ms (25.95%), Memory: 19.9 MB (9.28%) - Gitcode

--- 557-reverse-words-in-a-string-iii/557. Reverse Words in a String III.py
+++ 557-reverse-words-in-a-string-iii/557. Reverse Words in a String III.py
@@ -1,12 +1,10 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        f = b = 0
         arr = s.split(' ')
         ans = []
 
         for i in range(len(arr)):
             word = arr[i]
-            # print(word)
             end = len(word) - 1
             while end >= 0:
                 ans.append(word[end])
