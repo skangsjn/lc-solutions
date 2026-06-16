be56d6e79335b0c1a72d13a40198d4535ca4aa0b
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-15 21:36:45 -0400

    Runtime: 15 ms (12.63%), Memory: 20.4 MB (89.28%) - Gitcode

---
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        j = len(nums)-1
+        while i < j:
+            if nums[i] == 0:
+                nums.pop(i)
+                nums.append(0)
+                j -= 1
+            else:
+                i += 1
+            # print(nums) 
