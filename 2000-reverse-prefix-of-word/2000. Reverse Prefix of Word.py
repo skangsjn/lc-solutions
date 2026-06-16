cbbc0dbd1b5d85eca881a865243e42531e9eb0bc
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-16 13:46:09 -0400

    Runtime: 0 ms (100.00%), Memory: 19.2 MB (88.42%) - Gitcode

--- 2000-reverse-prefix-of-word/2000. Reverse Prefix of Word.py
+++ 2000-reverse-prefix-of-word/2000. Reverse Prefix of Word.py
@@ -1,23 +1,3 @@
-# class Solution:
-#     def reversePrefix(self, word: str, ch: str) -> str:
-#         idx = 0 
-#         stack = []
-#         ans = []
-
-#         while idx < len(word):
-#             stack.append(word[idx])
-#             if word[idx] == ch:
-#                 while stack:
-#                     ans.append(stack.pop())
-#                 idx += 1
-#                 while idx < len(word):
-#                     ans.append(word[idx])
-#                     idx += 1
-#                 return ''.join(ans)
-#             idx += 1
-        
-#         return word
-
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
         arr = list(word)
@@ -31,5 +11,3 @@ class Solution:
                     r -= 1
                 return ''.join(arr)
         return word
-
-        
