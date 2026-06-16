6f26d63004ec4247c191a3f6d7da0e1527516ab3
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-16 16:37:18 -0400

    Runtime: 5 ms (50.24%), Memory: 20.5 MB (61.49%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -8,14 +8,3 @@ class Solution:
             if nums[i] != 0:
                 nums[zero_ptr], nums[i] = nums[i], nums[zero_ptr]
                 zero_ptr +=1
-
-        # zero_idx = 0
-        # for i in range(len(nums)):
-        #     # print(nums[i])
-        #     if nums[i] == 0:
-        #         zero_idx = i
-        #     if nums[i] != 0 and nums[zero_idx] == 0:
-        #         nums[zero_idx], nums[i] = nums[i], nums[zero_idx]
-        #         zero_idx = i
-        #     print(nums)
-        #     print(zero_idx)
