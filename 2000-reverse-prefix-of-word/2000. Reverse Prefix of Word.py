19d0d58d42818417e908123b342300f4f4a7082f
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-16 13:43:52 -0400

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (18.55%) - Gitcode

--- 2000-reverse-prefix-of-word/2000. Reverse Prefix of Word.py
+++ 2000-reverse-prefix-of-word/2000. Reverse Prefix of Word.py
@@ -1,15 +1,21 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        arr = list(word)
-        l = 0
-        ch_bool = 0
+        idx = 0 
+        stack = []
+        ans = []
 
-        for r in range(len(arr)):
-            if arr[r] == ch and ch_bool == 0:
-                while l < r:
-                    arr[l], arr[r] = arr[r], arr[l]
-                    l += 1
-                    r -= 1
-                ch_bool = 1
+        while idx < len(word):
+            stack.append(word[idx])
+            if word[idx] == ch:
+                while stack:
+                    ans.append(stack.pop())
+                idx += 1
+                while idx < len(word):
+                    ans.append(word[idx])
+                    idx += 1
+                return ''.join(ans)
+            idx += 1
+        
+        return word
 
-        return ''.join(arr)
+        
