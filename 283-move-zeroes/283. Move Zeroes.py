1e44b28766d1a81747ace8bc5984c3466bed6726
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-15 22:16:16 -0400

    Runtime: 1 ms (88.53%), Memory: 20.6 MB (23.64%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -3,13 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i = 0
-        j = len(nums)-1
-        while i < j:
-            if nums[i] == 0:
-                nums.pop(i)
-                nums.append(0)
-                j -= 1
-            else:
-                i += 1
-            # print(nums) 
+        zero_ptr = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[zero_ptr], nums[i] = nums[i], nums[zero_ptr]
+                zero_ptr +=1
+
+        # zero_idx = 0
+        # for i in range(len(nums)):
+        #     # print(nums[i])
+        #     if nums[i] == 0:
+        #         zero_idx = i
+        #     if nums[i] != 0 and nums[zero_idx] == 0:
+        #         nums[zero_idx], nums[i] = nums[i], nums[zero_idx]
+        #         zero_idx = i
+        #     print(nums)
+        #     print(zero_idx)
