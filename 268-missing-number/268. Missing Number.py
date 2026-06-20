96f6c9a079820047da276f53bb8aab496a6821ec
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-20 15:40:39 -0400

    Runtime: 0 ms (100.00%), Memory: 20.9 MB (11.63%) - Gitcode

--- 268-missing-number/268. Missing Number.py
+++ 268-missing-number/268. Missing Number.py
@@ -1,5 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:        
-        for i in range(len(nums)+1):
-            if i not in nums:
+        num_set = set(nums)
+        n = len(nums) + 1
+        for i in range(n):
+            if i not in num_set:
                 return i
