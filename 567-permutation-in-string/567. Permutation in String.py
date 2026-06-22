4c2c2497ce60aaa4921f8e99327d9e21ac877f84
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-22 19:42:13 -0400

    Runtime: 1225 ms (12.11%), Memory: 19.6 MB (18.19%) - Gitcode

---
+++ 567-permutation-in-string/567. Permutation in String.py
@@ -0,0 +1,31 @@
+from collections import Counter, defaultdict
+
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n, m = len(s1), len(s2)
+        chars1 = Counter(s1)
+        chars2 = defaultdict(int)
+
+        if n > len(s2):
+            return False
+
+        for i in range(n):
+            chars2[s2[i]] += 1
+        
+        if chars1 == chars2:
+            return True
+        
+        l = 1
+        for r in range(n, len(s2)):
+            chars2 = Counter(s2[l:r+1])
+            print(chars2)
+            if chars1 == chars2:
+                return True
+            l += 1
+
+        
+        
+        
+        return False
+            
+        
