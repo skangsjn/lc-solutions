528bdd28023edc9b5ade2f8adac855b24527efdc
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-21 23:07:44 -0400

    Runtime: 13 ms (31.29%), Memory: 20 MB (88.91%) - Gitcode

--- 451-sort-characters-by-frequency/451. Sort Characters By Frequency.py
+++ 451-sort-characters-by-frequency/451. Sort Characters By Frequency.py
@@ -1,7 +1,7 @@
 class Solution:
     def frequencySort(self, s: str) -> str:
         counts = {}
-        ans = ''
+        ans = []
 
         for c in s:
             counts[c] = counts.get(c, 0) + 1
@@ -9,7 +9,6 @@ class Solution:
         sorted_counts = sorted(counts.items(), key=lambda item: item[1], reverse=True)
 
         for k, v in sorted_counts:
-            for i in range(v):
-                ans += k
+            ans.append(k*v)
 
-        return ans
+        return ''.join(ans)
