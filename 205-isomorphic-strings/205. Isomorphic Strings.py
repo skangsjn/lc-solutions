4ebbe60706b80556f7a084eeeb8087480657439b
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-24 19:38:54 -0400

    Runtime: 1 ms (95.73%), Memory: 19.2 MB (46.16%) - Gitcode

--- 205-isomorphic-strings/205. Isomorphic Strings.py
+++ 205-isomorphic-strings/205. Isomorphic Strings.py
@@ -1,12 +1,29 @@
 from collections import defaultdict
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_dic = defaultdict(list)
-        t_dic = defaultdict(list)
+        # try to map letters
+        mpp = {}
 
-        for i in range(len(s)):
-            s_dic[s[i]].append(i)
-        for i in range(len(t)):
-            t_dic[t[i]].append(i)
+        for s_char, t_char in zip(s, t):
+            if (s_char not in mpp) and (t_char not in mpp.values()):
+                mpp[s_char] = t_char
+            elif (s_char not in mpp) and (t_char in mpp.values()):
+                return False
+            elif (s_char in mpp) and mpp[s_char] != t_char:
+                return False
+            # else: 
+            #     return False
 
-        return sorted(s_dic.values()) == sorted(t_dic.values())
+        print(mpp)
+        return True
+
+        # s_dic = defaultdict(list)
+        # t_dic = defaultdict(list)
+
+        # for i in range(len(s)):
+        #     s_dic[s[i]].append(i)
+        # for i in range(len(t)):
+        #     t_dic[t[i]].append(i)
+
+        # return sorted(s_dic.values()) == sorted(t_dic.values())
