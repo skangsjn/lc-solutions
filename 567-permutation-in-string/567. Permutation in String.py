1a1a0f008a103cdb22fecdea2bf0f7ced17759c5
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-24 19:19:11 -0400

    Runtime: 23 ms (47.92%), Memory: 19.4 MB (52.94%) - Gitcode

--- 567-permutation-in-string/567. Permutation in String.py
+++ 567-permutation-in-string/567. Permutation in String.py
@@ -3,33 +3,45 @@ from collections import Counter, defaultdict
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         n, m = len(s1), len(s2)
-        chars1 = Counter(s1)
-        chars2 = defaultdict(int)
 
-        if n > len(s2):
+        if n > m:
             return False
-
-        for i in range(n):
-            chars2[s2[i]] += 1
         
-        if chars1 == chars2:
+        chars1 = Counter(s1)
+        need = len(chars1)
+        
+        for i in range(n):
+            letter = s2[i]
+            if letter in chars1:
+                chars1[letter] -= 1
+                if chars1[letter] == 0:
+                    need -= 1
+        
+        if need == 0:
             return True
         
         l = 0
         for r in range(n, len(s2)):
-            chars2[s2[r]] += 1
-            chars2[s2[l]] -= 1
-            if chars2[s2[l]] == 0:
-                del chars2[s2[l]]
+            left = s2[l]
+            if left in chars1:
+                if chars1[left] == 0:
+                    need += 1
+                chars1[left] += 1
             l += 1
 
-            # chars2 = Counter(s2[l:r+1])
-            if chars1 == chars2:
+            right = s2[r]
+            if right in chars1:
+                chars1[right] -= 1
+                if chars1[right] == 0:
+                    need -= 1
+
+            if need == 0:
                 return True
 
-        
-        
-        
         return False
             
+        # s1 = "abb", s2 = "eidbbbcccbbaooo"
         
+        # need = 2 unique characters
+
+        # {"a" : 1, "b" : 1 }
