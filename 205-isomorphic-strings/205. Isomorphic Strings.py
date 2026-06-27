fd408636600a090409f6eda59b909e4fb7fb8aa5
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-06-26 20:19:02 -0400

    Runtime: 3 ms (91.44%), Memory: 19.3 MB (46.24%) - Gitcode

--- 205-isomorphic-strings/205. Isomorphic Strings.py
+++ 205-isomorphic-strings/205. Isomorphic Strings.py
@@ -3,14 +3,16 @@ from collections import defaultdict
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         # try to map letters
-        mpp = {}
+        mpp_s = {}
+        mpp_t = {}
 
         for s_char, t_char in zip(s, t):
-            if (s_char not in mpp) and (t_char not in mpp.values()):
-                mpp[s_char] = t_char
-            elif (s_char not in mpp) and (t_char in mpp.values()):
+            if (s_char not in mpp_s) and (t_char not in mpp_t):
+                mpp_s[s_char] = t_char
+                mpp_t[t_char] = s_char
+            elif (s_char not in mpp_s) and (t_char in mpp_t):
                 return False
-            elif (s_char in mpp) and mpp[s_char] != t_char:
+            elif (s_char in mpp_s) and mpp_s[s_char] != t_char:
                 return False
         return True
             
