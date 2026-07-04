058e12e5a204ccdada49d8dd893091754958b368
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-04 16:48:56 -0400

    Runtime: 221 ms (5.08%), Memory: 42.4 MB (74.89%) - Gitcode

---
+++ 234-palindrome-linked-list/234. Palindrome Linked List.py
@@ -0,0 +1,34 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head.next is None:
+            return True
+
+        slow = fast = head
+        while fast and fast.next:
+            con = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        if fast:
+            slow = slow.next
+        
+        curr, prev = slow, None
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        
+        left, right = head, prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
