3ad6ca8f2c6873f1bf53df0eef4a0afc58739829
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-04 21:43:02 -0400

    Runtime: 335 ms (1.60%), Memory: 23.9 MB (9.67%) - Gitcode

---
+++ 328-odd-even-linked-list/328. Odd Even Linked List.py
@@ -0,0 +1,42 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        curr = head
+        con = head.next
+        prev_odd = ListNode(1)
+        prev_even = ListNode(2)
+
+        while curr and curr.next:
+            next_node = curr.next
+            next_odd = curr.next.next
+            
+            prev_odd.next = curr
+            prev_even.next = next_node
+
+            prev_odd = curr
+            prev_even = curr.next
+
+            curr = next_odd
+        
+        # print(curr)
+        if curr:
+            last_odd = prev_even.next
+            prev_odd.next = last_odd
+            prev_even.next = None
+            last_odd.next = con
+        else:
+            prev_odd.next = con
+            print(prev_odd)
+            # print(prev_even)
+            print(con)
+            # print(head)
+        
+        return head
+        # print(con)
+        # print(curr)
