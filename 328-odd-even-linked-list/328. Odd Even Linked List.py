a0f562fcbf6b32399cd84586a3bcea65d1eae050
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-04 21:46:06 -0400

    Runtime: 2 ms (20.19%), Memory: 21 MB (74.09%) - Gitcode

--- 328-odd-even-linked-list/328. Odd Even Linked List.py
+++ 328-odd-even-linked-list/328. Odd Even Linked List.py
@@ -7,29 +7,18 @@ class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head or not head.next:
             return head
-        curr = head
-        con = head.next
-        prev_odd = ListNode(1)
-        prev_even = ListNode(2)
 
-        while curr and curr.next:
-            next_node = curr.next
-            next_odd = curr.next.next
+        odd = head
+        even = head.next
+        even_head = even
+
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
             
-            prev_odd.next = curr
-            prev_even.next = next_node
+            even.next = odd.next
+            even = even.next
 
-            prev_odd = curr
-            prev_even = curr.next
-
-            curr = next_odd
-        
-        if curr:
-            last_odd = prev_even.next
-            prev_odd.next = last_odd
-            prev_even.next = None
-            last_odd.next = con
-        else:
-            prev_odd.next = con
+        odd.next = even_head
         
         return head
