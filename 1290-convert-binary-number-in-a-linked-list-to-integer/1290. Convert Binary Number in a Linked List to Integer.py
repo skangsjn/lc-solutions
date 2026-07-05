408ac07fdf038168800619388b2c9050f2883458
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-04 20:12:04 -0400

    Runtime: 0 ms (100.00%), Memory: 19.3 MB (47.62%) - Gitcode

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290. Convert Binary Number in a Linked List to Integer.py
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290. Convert Binary Number in a Linked List to Integer.py
@@ -5,24 +5,11 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
-        fast = slow = head
-        power = 0
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            power += 1
-        if fast:
-            power = power*2
-        else:
-            power = (power*2) - 1
-        
-        curr = head
         ans = 0
-        while power >= 0:
-            print(curr, power)
-            if curr.val == 1:
-                ans += (2 ** power)
+        curr = head
+
+        while curr:
+            ans = ans * 2 + curr.val
             curr = curr.next
-            power -= 1
         
         return ans
