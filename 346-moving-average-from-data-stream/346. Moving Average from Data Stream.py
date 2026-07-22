46ac99535a62d10aecca2628601b711c7869b7c1
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-21 20:47:41 -0400

    Runtime: 17 ms (31.87%), Memory: 22.3 MB (41.71%) - Gitcode

---
+++ 346-moving-average-from-data-stream/346. Moving Average from Data Stream.py
@@ -0,0 +1,19 @@
+from collections import deque
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.queue = deque()
+        self.size = size
+
+    def next(self, val: int) -> float:
+        self.queue.append(val)
+
+        while self.queue and len(self.queue) > self.size:
+            self.queue.popleft()
+
+        return sum(self.queue) / len(self.queue)
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
