9e8748db7f312be4f54b0e166afc26096d94eb40
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-21 20:53:01 -0400

    Runtime: 7 ms (55.92%), Memory: 22.4 MB (20.70%) - Gitcode

--- 346-moving-average-from-data-stream/346. Moving Average from Data Stream.py
+++ 346-moving-average-from-data-stream/346. Moving Average from Data Stream.py
@@ -4,14 +4,19 @@ class MovingAverage:
     def __init__(self, size: int):
         self.queue = deque()
         self.size = size
+        # elements seen so far
+        self.window_sum = 0
+        self.count = 0
 
     def next(self, val: int) -> float:
         self.queue.append(val)
+        self.count += 1
 
-        while self.queue and len(self.queue) > self.size:
-            self.queue.popleft()
+        # always popleft after we've seen the first {size} elements
+        tail = self.queue.popleft() if self.count > self.size else 0
+        self.window_sum = self.window_sum - tail + val
 
-        return sum(self.queue) / len(self.queue)
+        return self.window_sum / min(self.size, self.count)
 
 
 # Your MovingAverage object will be instantiated and called as such:
