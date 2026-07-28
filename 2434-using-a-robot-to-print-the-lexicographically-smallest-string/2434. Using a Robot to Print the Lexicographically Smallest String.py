08126ac207fda38bb57f97f2d31ddcdc5a3b75c7
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-27 20:31:44 -0400

    Runtime: 1573 ms (5.12%), Memory: 22.1 MB (62.60%) - Gitcode

---
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434. Using a Robot to Print the Lexicographically Smallest String.py
@@ -0,0 +1,26 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        freq = [0] * 26
+        t = []
+        p = []
+
+        def find_smallest():
+            for i in range(len(freq)):
+                if freq[i] > 0:
+                    return i
+            return 26
+
+        for c in s:
+            freq[ord(c) - ord('a')] += 1
+
+        for c in s:
+            freq[ord(c) - ord('a')] -= 1
+            t.append(c)
+            while t and (ord(t[-1]) - ord('a')) <= find_smallest():
+                p.append(t.pop())
+
+        while t:
+            p.append(t.pop())
+        
+        return ''.join(p)
+        
