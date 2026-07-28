6e6d1f8769d8779343aef2bf0802341d3f38f4e7
Sarah Kang <134986232+skangsjn@users.noreply.github.com>
2026-07-27 21:54:39 -0400

    Runtime: 3 ms (45.28%), Memory: 19.3 MB (87.97%) - Gitcode

---
+++ 946-validate-stack-sequences/946. Validate Stack Sequences.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        i = 0
+        stack = []
+
+        for num in pushed:
+            stack.append(num)
+            while stack and i < len(popped) and stack[-1] == popped[i]:
+                stack.pop()
+                i += 1
+            
+        return i == len(popped)
